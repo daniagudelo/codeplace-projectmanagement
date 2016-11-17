49c3ce60ff5ac4a6db43846ef66f97160aeb1702
daniagudelo <daniagudelo@gmail.com>
2016-11-17 21:14:23 +0000

    Added auto html

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ActiveRecord::Base
     belongs_to :user
+    auto_html_for :content do
+        html_escape
+        youtube(width: '100%', height: 250)
+        image
+        link target: '_blank', rel: 'nofollow'
+        simple_format
+    end
 end
